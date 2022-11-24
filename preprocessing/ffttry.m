a95404eba034e9e6e6986a31b1ace231fd412774
maurice1128 <117107598+maurice1128@users.noreply.github.com>
2022-11-24 21:36:12 +0800

    Update ffttry.m

--- preprocessing/ffttry.m
+++ preprocessing/ffttry.m
@@ -1,32 +1,35 @@
 
-
+clc
 mat_file = '/Users/mauricewang/Desktop/CL-DBS_4/mat_file_no_zscore'
 
 
 contrast_power = []
 
 
-for i = 1:96
-    load([mat_file '/Re0' num2str(i) '_CL-DBS_4'])
-    for sec = 11:880
+for i = 1
+    i
+    load([mat_file '/Re0' num2str(i) '_CL-DBS_4']);
+    for sec = 21:870
 
-
-x = EEG.Data(1,:)
-x = x(1000*(i)-10000:1000*(i)+10000)
-n = length(x)
-fs = 1000
-y = fft(x,length(n))
+sec;
+x = EEG.Data(1,:);
+x = x(1000*(sec)-20000+1:1000*(sec)+20000);
+n = length(x);
+fs = 1000;
+y = fft(x,n);
 f = (0:n-1)*(fs/n);
 power = abs(y).^2/n;   
 
-NREM = sum(power(9:81))
-REM = sum(power(101:173))
-contrast = NREM/ REM
-contrast_power = [contrast_power , contrast]
+NREM = sum(power(17:161));
+REM = sum(power(201:345));
+contrast = NREM/ REM;
+contrast_power = [contrast_power , contrast];
 
     end
 end
 
-axis([11 880 0 2])
+
+plot(21:870 , contrast_power);
+
+axis([21 870 0 2]);
 grid on
-plot(11:880 , contrast_power)
